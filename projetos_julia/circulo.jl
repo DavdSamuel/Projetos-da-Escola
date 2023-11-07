ab5d4f9aa8959d65c86e7269d4ac86c707cd80f9
David Samuel <139819654+DavdTheItGuy@users.noreply.github.com>
2023-11-07 15:04:41 +0000

    Correção

--- projetos_julia/circulo.jl
+++ projetos_julia/circulo.jl
@@ -3,4 +3,4 @@ diâmetro = parse(Float64, readline())  # Lendo e convertendo para número
 raio = diâmetro / 2  # Dividindo o diâmetro por 2
 println("Ok, o raio é $raio")
 círculo = π * raio^2  # Área do círculo
-println("O cálculo da circunferência é $círculo")  # imprime o resultado
+println("O cálculo da circunferência é $círculo")  # Imprime o resultado
