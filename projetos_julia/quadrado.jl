130f9e7dad167e31e828784472de7ebe33581ad1
David Samuel <139819654+DavdTheItGuy@users.noreply.github.com>
2023-11-07 15:09:10 +0000

    Correção

--- projetos_julia/quadrado.jl
+++ projetos_julia/quadrado.jl
@@ -1,5 +1,5 @@
 println("Qual o comprimento/largura do quadrado: ") # Pergunta qual é o comprimento/largura do quadrado
 comprimento = parse(Float64, readline())  # Lendo e convertendo para número
 println("Ok, o comprimento/largura é $comprimento")
-quadrado = comprimento^2  # Multiplicando o comprimento por 2
+quadrado = comprimento^2  # Multiplicando o comprimento por ele mesmo
 println("O cálculo do quadrado é $quadrado")  # Imprime o resultado
