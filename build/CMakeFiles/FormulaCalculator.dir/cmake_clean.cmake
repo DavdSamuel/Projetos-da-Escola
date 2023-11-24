41facb584b62bb6c6b114104adb650249258438c
David Samuel <139819654+DavdTheItGuy@users.noreply.github.com>
2023-11-24 17:42:02 +0000

    Binários de configuração do CMake

---
+++ build/CMakeFiles/FormulaCalculator.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FormulaCalculator.dir/src/main.c.o"
+  "FormulaCalculator"
+  "FormulaCalculator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/FormulaCalculator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
