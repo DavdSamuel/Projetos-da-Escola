2c1fb6d4ae39076d6793b39a38e3dcbdf1179b4e
David Samuel <139819654+DavdTheItGuy@users.noreply.github.com>
2023-11-24 23:56:38 +0000

    Protótipo de função de adição

--- FormulaCalculator/src/main.c
+++ FormulaCalculator/src/main.c
@@ -1,10 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
+/*
+argc: Argument counter, ou contador de argumentos
+*/
+int main(int argc, char *argv[])
 {
     printf("######################\n");
     printf("# Formula Calculator #\n");
     printf("######################\n");
+
+    if (argv[1] == "add")
+    {
+        if (argc == 2 || argc == 3) {
+            printf("ERRO!\nArgumentos insuficientes:\n");
+            printf("Uso correto do argumento \"add\": add <numero1> <numero2>\n");
+            return 1;
+        }
+        else if (argc > 3)
+        {
+
+        }
+    }
+    else
+    {
+        printf("ERRO!\nArgumento não reconhecido!\n");
+        return 1;
+    }
 
     return 0;
 }
