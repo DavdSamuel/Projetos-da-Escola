a108010ba071660a7b0a47f422725bbe1ac2ebc6
David Samuel <139819654+DavdTheItGuy@users.noreply.github.com>
2023-12-02 14:34:25 +0000

    Corrigindo a lógica de adição

--- FormulaCalculator/src/main.c
+++ FormulaCalculator/src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "operadores.h"
 
 int main(int argc, char *argv[])
@@ -7,7 +8,20 @@ int main(int argc, char *argv[])
     printf("# Formula Calculator #\n");
     printf("######################\n");
 
-    float teste = adicao(argc, argv);
+    if (argc <= 2 || strcmp(argv[1], "add") != 0)
+    {
+        printf("ERRO!\nArgumento não reconhecido ou insuficiente.\n");
+        return 1;
+    }
+    else if (argc <= 3 || strcmp(argv[1], "add") != 0) {
+        printf("ERRO!\nArgumentos insuficientes:\n");
+        printf("Uso correto do argumento \"add\": ./FormulaCalculator add <numero1> <numero2>\n");
+        return 1;
+    }
+    else if (argc == 4 || strcmp(argv[1], "add") != 0)
+    {
+        float resultado = adicao(argc, argv);
+    }
 
     return 0;
 }
--- FormulaCalculator/src/operadores.c
+++ FormulaCalculator/src/operadores.c
@@ -4,24 +4,10 @@
 
 float adicao(int argc, char *argv[])
 {
-    if (argc <= 2 || strcmp(argv[1], "add") != 0)
-    {
-        printf("ERRO!\nArgumento não reconhecido ou insuficiente.\n");
-        return 1;
-    }
-    else
-    {
-        if (argc <= 3) {
-            printf("ERRO!\nArgumentos insuficientes:\n");
-            printf("Uso correto do argumento \"add\": ./FormulaCalculator add <numero1> <numero2>\n");
-            return 1;
-        }
+    float n1 = atof(argv[2]);
+    float n2 = atof(argv[3]);
 
-        float n1 = atof(argv[2]);
-        float n2 = atof(argv[3]);
+    float resultado = n1 + n2;
 
-        float resultado = n1 + n2;
-
-        printf("Resultado: %.2f\nEndereço de memória:\nNúmero1: %p\nNúmero2: %p\nResultado: %p\n", resultado, (void *)&n1, (void *)&n2, (void *)&resultado);
-    }
+    printf("Resultado: %.2f\nEndereço de memória:\nNúmero1: %p\nNúmero2: %p\nResultado: %p\n", resultado, (void *)&n1, (void *)&n2, (void *)&resultado);
 } 
