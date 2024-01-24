7faf791d38d8562198f9dd12c5045a40cd720307
David Samuel <139819654+DavdSamuel@users.noreply.github.com>
2024-01-23 22:50:24 -0300

    Delete FormulaCalculator directory

--- FormulaCalculator/src/operadores.h
+++
@@ -1,8 +0,0 @@
-#ifndef OPERADORES
-
-void adicao(int argc, char *argv[]);
-void subtracao(int argc, char *argv[]);
-void multiplicacao(int argc, char *argv[]);
-void divisao(int argc, char *argv[]);
-
-#endif
